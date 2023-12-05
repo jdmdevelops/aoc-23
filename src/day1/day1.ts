3450d04855859a1fc9e16569ae4cb9b1b2f1f658
Justin Moore <justin.moore@coop.org>
2023-12-05 12:07:15 -0500

    stumped :/

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -2,19 +2,52 @@ import fs from "node:fs";
 
 const input = fs.readFileSync(`${import.meta.dir}/day1.txt`, "utf8");
 
+const arr = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+const regEx = new RegExp(arr.join("|"), "g");
+
+const legend: Record<string, string> = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
 //split by new line
 const lines = input.split(/\r?\n/);
 
 //determine which characters are numbers
 const data = lines.map((line) => {
-  const firstNum = line.split("").find((char) => !isNaN(+char)) ?? "";
-  const lastNum = line.split("").findLast((char) => !isNaN(+char)) ?? "";
+  // console.log(line);
+  const replacedNums = line.replace(regEx, (match) => legend[match]);
+  // console.log(replacedNums);
+
+  const firstNum = replacedNums.split("").find((char) => !isNaN(+char)) ?? "";
+  // console.log(firstNum);
+  const lastNum =
+    replacedNums.split("").findLast((char) => !isNaN(+char)) ?? "";
+  // console.log(lastNum);
 
   return firstNum + lastNum;
 });
 
-//convert each item into an int and sum
-const solution = data.reduce((acc, current) => acc + parseInt(current), 0);
+const solution = data.reduce((acc, current) => {
+  return +acc + +current;
+}, 0);
 
 export default () => {
   return console.log(solution);
